22adada26616209d5f4a7228336281a2d8b010b2
Andrey <trancemilly7@gmail.com>
2023-07-13 20:19:19 +0200

    adds 'why' section

--- src/App.jsx
+++ src/App.jsx
@@ -19,34 +19,55 @@ function App() {
 
         <button className='invite-btn'>Request Invite</button>
       </section>
-      <div className='under-test'></div>
-      {/* <img src='/images/bg-intro-desktop.svg'></img>
-      <img src='/images/image-mockups.png'></img> */}
+      <section className='why'>
+        <h1 className='why-title'>Why choose Easybank?</h1>
 
- 
+        <p className='why-text'> 
+          We leverage Open Banking to turn your bank account into your financial hub. Control 
+          your finances like never before.
+        </p>
+        <div className='why-items-container'>
+          <div className='why-item'>
+            <img className='why-item-icon' src='/images/icon-online.svg'></img>
+            <h2 className='why-item-title'>Online Banking</h2>
+            <p className='why-item-text'>
+              Our modern web and mobile applications allow you to keep track of your finances 
+              wherever you are in the world.
+            </p>
+          </div>
+          <div className='why-item'>
+            <img className='why-item-icon' src='/images/icon-budgeting.svg'></img>
+            <h2 className='why-item-title'>Simple Budgeting</h2>
+            <p className='why-item-text'>
+              See exactly where your money goes each month. Receive notifications when you’re 
+              close to hitting your limits.
+            </p>
+          </div>
+          <div className='why-item'>
+            <img className='why-item-icon' src='/images/icon-onboarding.svg'></img>
+            <h2 className='why-item-title'>Fast Onboarding</h2>
+            <p className='why-item-text'>
+              We don’t do branches. Open your account in minutes online and start taking control 
+              of your finances right away.
+            </p>
+          </div>
+          <div className='why-item'>
+            <img className='why-item-icon' src='/images/icon-api.svg'></img>
+            <h2 className='why-item-title'>Open API</h2>
+            <p className='why-item-text'>
+                Manage your savings, investments, pension, and much more from one account. Tracking 
+                your money has never been easier.
+            </p>
+          </div>
+        </div>
+      </section>
+  
+  
 
+  
+  
 
-
-  Why choose Easybank?
-
-  We leverage Open Banking to turn your bank account into your financial hub. Control 
-  your finances like never before.
-
-  Online Banking
-  Our modern web and mobile applications allow you to keep track of your finances 
-  wherever you are in the world.
-
-  Simple Budgeting
-  See exactly where your money goes each month. Receive notifications when you’re 
-  close to hitting your limits.
-
-  Fast Onboarding
-  We don’t do branches. Open your account in minutes online and start taking control 
-  of your finances right away.
-
-  Open API
-  Manage your savings, investments, pension, and much more from one account. Tracking 
-  your money has never been easier.
+  
 
   
 
